d9734d3df24fe47ed53a22d1e102d5a4cabcd5eb
chenshi028 <1161820598@qq.com>
2025-01-11 21:47:09 +0800

    add cannon by chenshi028

---
+++ src/battle_game/core/units/cannon_chenshi028.cpp
@@ -0,0 +1,172 @@
+#include "cannon_chenshi028.h"
+
+#include "battle_game/core/bullets/bullets.h"
+#include "battle_game/core/game_core.h"
+#include "battle_game/graphics/graphics.h"
+
+namespace battle_game::unit {
+
+namespace {
+uint32_t cannon_body_model_index = 0xffffffffu;
+uint32_t cannon_turret_model_index = 0xffffffffu;
+}  // namespace
+
+Cannon_chenshi028::Cannon_chenshi028(GameCore *game_core, uint32_t id, uint32_t player_id)
+    : Unit(game_core, id, player_id) {
+  if (!~cannon_body_model_index) {
+    auto mgr = AssetsManager::GetInstance();
+    {
+      /* Cannon Body */
+      cannon_body_model_index = mgr->RegisterModel(
+          {
+              {{-0.8f, -1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+              {{0.8f, -1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+              {{0.8f, 1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+              {{-0.8f, 1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+          },
+          {0, 1, 2, 3, 0, 2, 1, 3});
+    }
+
+    {
+      /* Cannon Turret */
+      std::vector<ObjectVertex> turret_vertices;
+      std::vector<uint32_t> turret_indices;
+      const int precision = 60;
+      const float inv_precision = 1.0f / float(precision);
+      for (int i = 0; i < precision; i++) {
+        auto theta = (float(i) + 0.5f) * inv_precision + 0.5f;
+        theta *= glm::pi<float>();
+        auto sin_theta = std::sin(theta);
+        auto cos_theta = std::cos(theta);
+        turret_vertices.push_back({{sin_theta * 0.5f, cos_theta * 0.5f},
+                                   {0.0f, 0.0f},
+                                   {0.7f, 0.7f, 0.7f, 1.0f}});
+        turret_indices.push_back(i);
+        turret_indices.push_back((i + 1) % precision);
+        turret_indices.push_back(precision);
+      }
+      turret_vertices.push_back(
+          {{0.0f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
+      turret_vertices.push_back(
+          {{-0.5f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
+      turret_vertices.push_back(
+          {{0.5f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
+      turret_vertices.push_back(
+          {{-0.5f, 1.2f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
+      turret_vertices.push_back(
+          {{0.5f, 1.2f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
+      turret_indices.push_back(precision + 1 + 0);
+      turret_indices.push_back(precision + 1 + 1);
+      turret_indices.push_back(precision + 1 + 2);
+      turret_indices.push_back(precision + 1 + 1);
+      turret_indices.push_back(precision + 1 + 2);
+      turret_indices.push_back(precision + 1 + 3);
+      cannon_turret_model_index =
+          mgr->RegisterModel(turret_vertices, turret_indices);
+    }
+  }
+}
+
+void Cannon_chenshi028::Render() {
+  battle_game::SetTransformation(position_, rotation_);
+  battle_game::SetTexture(0);
+  battle_game::SetColor(game_core_->GetPlayerColor(player_id_));
+  battle_game::DrawModel(cannon_body_model_index);
+  battle_game::SetColor({0.0f, 0.0f, 0.0f, 1.0f});
+  battle_game::DrawModel(cannon_turret_model_index);
+}
+
+void Cannon_chenshi028::Update() {
+  CannonMove(1.5f, glm::radians(90.0f));
+  Fire();
+  Shield();
+}
+
+void Cannon_chenshi028::CannonMove(float move_speed, float rotate_angular_speed) {
+  auto player = game_core_->GetPlayer(player_id_);
+  if (player) {
+    auto &input_data = player->GetInputData();
+    glm::vec2 offset{0.0f};
+    if (input_data.key_down[GLFW_KEY_W]) {
+      offset.y += 1.0f;
+    }
+    if (input_data.key_down[GLFW_KEY_S]) {
+      offset.y -= 1.0f;
+    }
+    float speed = move_speed * GetSpeedScale();
+    offset *= kSecondPerTick * speed;
+    auto new_position =
+        position_ + glm::vec2{glm::rotate(glm::mat4{1.0f}, rotation_,
+                                          glm::vec3{0.0f, 0.0f, 1.0f}) *
+                              glm::vec4{offset, 0.0f, 0.0f}};
+    if (!game_core_->IsBlockedByObstacles(new_position)) {
+      game_core_->PushEventMoveUnit(id_, new_position);
+    }
+    float rotation_offset = 0.0f;
+    if (input_data.key_down[GLFW_KEY_A]) {
+      rotation_offset += 1.0f;
+    }
+    if (input_data.key_down[GLFW_KEY_D]) {
+      rotation_offset -= 1.0f;
+    }
+    rotation_offset *= kSecondPerTick * rotate_angular_speed * GetSpeedScale();
+    game_core_->PushEventRotateUnit(id_, rotation_ + rotation_offset);
+  }
+}
+
+void Cannon_chenshi028::Fire() {
+  if (fire_count_down_ == 0) {
+    auto player = game_core_->GetPlayer(player_id_);
+    if (player) {
+      auto &input_data = player->GetInputData();
+      if (input_data.mouse_button_down[GLFW_MOUSE_BUTTON_LEFT]) {
+        auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, rotation_);
+        GenerateBullet<bullet::CannonBall>(
+            position_ + Rotate({0.0f, 1.2f}, rotation_),
+            rotation_, GetDamageScale() * 5, velocity);
+        fire_count_down_ = kTickPerSecond * 3;  // Fire interval 3 seconds.
+      }
+    }
+  }
+  if (fire_count_down_) {
+    fire_count_down_--;
+  }
+}
+
+void Cannon_chenshi028::Shield() {
+  if (shield_count_down_ == 0) {
+    auto player = game_core_->GetPlayer(player_id_);
+    if (player) {
+      auto &input_data = player->GetInputData();
+      if (input_data.mouse_button_down[GLFW_KEY_SPACE]) {
+        shield_ = kTickPerSecond * 2; // Shield validity period 2 seconds.
+        shield_count_down_ = kTickPerSecond * 10;  // Shield interval 10 seconds.
+      }
+    }
+  }
+  if (shield_) {
+    shield_--;
+  }
+  if (shield_count_down_) {
+    shield_count_down_--;
+  }
+}
+
+bool Cannon_chenshi028::IsHit(glm::vec2 position) const {
+  if (shield_ && fire_count_down_) {
+    return false;
+  }
+  position = WorldToLocal(position);
+  return position.x > -0.8f && position.x < 0.8f && position.y > -1.0f &&
+         position.y < 1.0f && position.x + position.y < 1.6f &&
+         position.y - position.x < 1.6f;
+}
+
+const char *Cannon_chenshi028::UnitName() const {
+  return "Cannon";
+}
+
+const char *Cannon_chenshi028::Author() const {
+  return "chenshi028";
+}
+}  // namespace battle_game::unit
